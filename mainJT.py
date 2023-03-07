49c1f2dea720fdbe8aae71ae529fa52b057afef3
JasonTor <jasontorres999@hotmail.com>
2023-03-07 15:39:43 +0100

    commit jason

--- mainJT.py
+++ mainJT.py
@@ -30,7 +30,6 @@ df2 = pd.DataFrame(np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]]),
 
 ve = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
 
-print(np.mean(ve, 0))
 
 """
 print(df2)
@@ -43,7 +42,7 @@ print(df2.loc[df2["a"] >= 4, :])
 
 nlp = spacy.load("en_core_web_md")
 test = [doc for doc in nlp.pipe(
-    DC_US_GP["Content"].iloc[:10].astype(str), disable=['parser', 'ner'])]  # liste des 10 premiers commentaires
+    DC_US_GP["Content"].iloc[:200].astype(str), disable=['parser', 'ner'])]  # liste des 10 premiers commentaires
 
 
 def list_vectb(phrase):
@@ -62,6 +61,8 @@ def phrase_to_vec(list_vect):  # renvoie le vecteur d'une phrase
         if sum(v) != 0:
             res += v
             n += 1
+    if n == 0:
+        return res + np.ones(300)/100
     return res/n
 
 
@@ -78,29 +79,29 @@ print(test[0][0].text)
 X = list_vect(test)
 
 kclusterer = KMeansClusterer(
-    2, distance=nltk.cluster.util.cosine_distance, repeats=25)
+    2, distance=nltk.cluster.util.cosine_distance, repeats=25, avoid_empty_clusters=True)
 clusters = kclusterer.cluster(X, assign_clusters=True)
 print(clusters)
 
 # for index, phrase in enumerate(phrases):
 #     print (str(clusters[index]) + ":" + str(phrase))
 
-"""
+
 kmeans = cluster.KMeans(n_clusters=2)
 kmeans.fit(X)
 
 
-model = TSNE(n_components=2, random_state=0)
+model = TSNE(n_components=2, perplexity=6.0, random_state=0)
 np.set_printoptions(suppress=True)
 
-Y = model.fit_transform(X)
-plt.scatter(Y[:, 0], Y[:, 1], c=clusters, s=290, alpha=.5)
+Y = model.fit_transform(np.array(X))
+plt.scatter(Y[:, 0], Y[:, 1], c=clusters, s=200, alpha=.5)
 
 plt.show()
 
 # labels = kmeans.labels_
 # centroids = kmeans.cluster_centers_
-"""
+
 
 # print(test[0])
 # print(test[0][6])
